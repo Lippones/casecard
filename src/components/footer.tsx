06c0535719b43edfc14f475e0f97a2c6e7b531ab
Filipe Vieira <filipe.silva@bttis.com>
2024-10-11 11:49:07 -0300

    fix: mobile view

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,12 +3,13 @@ import { Separator } from './ui/separator'
 import { Button } from './ui/button'
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 export function Footer() {
   return (
     <footer className="border-t">
       <div className="container px-4 md:px-8 py-20 mx-auto space-y-14">
-        <div className="grid grid-cols-4 gap-12">
+        <div className="grid md:grid-cols-4 gap-12">
           <div className="col-span-1">
             <h3 className="text-2xl font-bold">Casecard</h3>
           </div>
@@ -22,7 +23,7 @@ export function Footer() {
 
         <Separator />
 
-        <div className="flex gap-16">
+        <div className="flex  gap-16">
           <div>
             <h3 className="uppercase">Links úteis</h3>
             <ul className="space-y-2 mt-4 text-muted-foreground">
@@ -61,11 +62,27 @@ export function Footer() {
           </div>
         </div>
 
-        <div className="flex items-center justify-between">
-          <span className="text-sm text-muted-foreground">
-            © 2021 Casecard. Todos os direitos reservados.
-          </span>
-          <div className="flex items-center justify-between rounded-full border bg-background/60 px-6 py-3 backdrop-blur-md gap-12">
+        <div className="flex  max-md:flex-col-reverse gap-8 items-center justify-between">
+          <div className="space-y-6 max-sm:w-full rounded-full border bg-secondary/40 px-6 py-2 backdrop-blur-md">
+            <div className="flex items-center gap-6">
+              <span className="text-sm">
+                Desenvolvido por{' '}
+                <Link
+                  href="https://www.lippe.dev"
+                  className="underline font-medium">
+                  Filipe Vieira
+                </Link>
+              </span>
+              <Avatar>
+                <AvatarFallback>Lippe</AvatarFallback>
+                <AvatarImage src="https://avatars.githubusercontent.com/u/34816616?v=4" />
+              </Avatar>
+            </div>
+            <span className="text-xs text-muted-foreground">
+              © 2021 Casecard. Todos os direitos reservados.
+            </span>
+          </div>
+          <div className="flex max-sm:w-full items-center justify-between rounded-full border bg-secondary/40 px-6 py-4 backdrop-blur-md gap-12">
             <span className="font-semibold">Follow me</span>
             <ul className="flex items-center gap-2">
               <li>
