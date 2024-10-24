b97d6b67825fbedcaa8d2625b3462222fb4f1c43
Filipe Vieira <filipe68ft@hotmail.com>
2024-10-24 15:22:52 -0300

    feat: internalizing success page

--- src/app/success/[id]/page.tsx
+++ src/app/success/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Preview } from './preview'
 import { Suspense } from 'react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { getTranslations } from 'next-intl/server'
 
 export default async function Success(props: {
   params: Promise<{
@@ -14,26 +15,26 @@ export default async function Success(props: {
 
   const { purchase } = await getPurchase(params.id)
 
+  const t = await getTranslations('success')
+
   if (!purchase) {
-    return <h1>Não foi possivel encontrar sua compra</h1>
+    return <h1>{t('error')}</h1>
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-screen-lg mx-auto space-y-6">
+    <div className="flex flex-col items-center px-4 justify-center min-h-svh max-w-screen-lg mx-auto space-y-6">
       <Suspense>
         <Confetti />
       </Suspense>
-      <h1 className="text-primary font-medium text-xl">Thank you!</h1>
-      <p className="text-4xl font-semibold">Purchase Completed Successfully!</p>
+      <h1 className="text-primary font-medium text-xl">{t('title')}</h1>
+      <p className="text-4xl font-semibold text-center">{t('description')}</p>
       {purchase.deliveryMethod === 'email' && (
-        <p className="text-muted-foreground">
-          The sticker has been sent to your email
-        </p>
+        <p className="text-muted-foreground">{t('message.email')}</p>
       )}
       <div className="max-w-[700px] w-full">
         <Preview accessKey={purchase.accessKey} />
         <Button size={'lg'} className="mt-6 w-full" asChild>
-          <Link href="/">Back to site</Link>
+          <Link href="/">{t('backButton')}</Link>
         </Button>
       </div>
     </div>
