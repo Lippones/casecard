1d3120ae76873819ce86d68ffb20fe63c3f764af
Filipe Vieira <filipe68ft@hotmail.com>
2024-10-24 16:11:06 -0300

    feat: auto detect language

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,40 @@
 import { getRequestConfig } from 'next-intl/server'
+import { cookies, headers } from 'next/headers'
+
+const supportedLocales = ['en', 'pt']
+
+function getPrimaryLocale(acceptedLanguages: string[]) {
+  for (const lang of acceptedLanguages) {
+    const baseLang = lang.split('-')[0]
+    if (supportedLocales.includes(baseLang)) {
+      return baseLang
+    }
+  }
+  return 'en'
+}
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = 'en'
+  const cookie = await cookies()
+  const header = await headers()
+
+  let locale = cookie.get('locale')?.value
+
+  if (!locale) {
+    const acceptedLanguages = header.get('accept-language')?.split(',') || [
+      'en',
+    ]
+
+    locale = getPrimaryLocale(acceptedLanguages)
+
+    cookie.set({
+      value: locale,
+      name: 'locale',
+    })
+  }
+
+  if (!supportedLocales.includes(locale)) {
+    locale = 'en'
+  }
 
   return {
     locale,
